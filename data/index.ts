2d3177c9b784bbb1de5ff522056095aedc781501
Shaurya Rai <raishaurya50@gmail.com>
2024-05-27 08:31:41 +0530

    first commit

---
+++ data/index.ts
@@ -0,0 +1,154 @@
+export const navItems = [
+    { name: "About", link: "#about" },
+    { name: "Projects", link: "#projects" },
+    { name: "Testimonials", link: "#testimonials" },
+    { name: "Contact", link: "#contact" },
+  ];
+  
+  export const gridItems = [
+    {
+      id: 1,
+      title: "I prioritize client collaboration, fostering open communication ",
+      description: "",
+      className: "lg:col-span-3 md:col-span-6 md:row-span-4 lg:min-h-[60vh]",
+      imgClassName: "w-full h-full",
+      titleClassName: "justify-end",
+      img: "/b1.svg",
+      spareImg: "",
+    },
+    {
+      id: 2,
+      title: "I'm very flexible with time zone communications",
+      description: "",
+      className: "lg:col-span-2 md:col-span-3 md:row-span-2",
+      imgClassName: "",
+      titleClassName: "justify-start",
+      img: "",
+      spareImg: "",
+    },
+    {
+      id: 3,
+      title: "My tech stack",
+      description: "I constantly try to improve",
+      className: "lg:col-span-2 md:col-span-3 md:row-span-2",
+      imgClassName: "",
+      titleClassName: "justify-center",
+      img: "",
+      spareImg: "",
+    },
+    {
+      id: 4,
+      title: "Tech enthusiast with a passion for development.",
+      description: "",
+      className: "lg:col-span-2 md:col-span-3 md:row-span-1",
+      imgClassName: "",
+      titleClassName: "justify-start",
+      img: "/grid.svg",
+      spareImg: "/b4.svg",
+    },
+    {
+      id: 5,
+      title: "Currently working on my skillset of development and content writing",
+      description: "The Inside Scoop",
+      className: "md:col-span-3 md:row-span-2",
+      imgClassName: "absolute right-0 bottom-0 md:w-96 w-60",
+      titleClassName: "justify-center md:justify-start lg:justify-center",
+      img: "/b5.svg",
+      spareImg: "/grid.svg",
+    },
+    {
+      id: 6,
+      title: "Do you want to start a project together?",
+      description: "",
+      className: "lg:col-span-2 md:col-span-3 md:row-span-1",
+      imgClassName: "",
+      titleClassName: "justify-center md:max-w-full max-w-60 text-center",
+      img: "",
+      spareImg: "",
+    },
+  ];
+  
+  export const projects = [
+    {
+      id: 1,
+      title: "Copy Writing a Book",
+      des: "This was a book which I copywrited as per the client's Instructions.",
+      img: "/1000.jpg",
+      iconLists: ["/re.svg", "/tail.svg", "/ts.svg", "/three.svg", "/fm.svg"],
+    },
+    {
+      id: 2,
+      title: "Breadit - Law Learners",
+      des: "This website was built by me as per the instructions of the client for a place where curious law learners could ask doubts about their work.",
+      img: "/200.jpg",
+      iconLists: ["/re.svg", "/tail.svg", "/ts.svg", "/three.svg", "/fm.svg"],
+      link: 'https://learn-law-another-one.vercel.app/'
+    },
+    {
+      id: 3,
+      title: "Animated Apple Iphone 3D Website",
+      des: "Recreated the Apple iPhone 15 Pro website, combining GSAP animations and Three.js 3D effects..",
+      img: "/p4.svg",
+      iconLists: ["/next.svg", "/tail.svg", "/ts.svg", "/three.svg", "/gsap.svg"],
+      link: "/ui.apple.com",
+    },
+    
+  ];
+  
+  export const testimonials = [
+    {
+      id: 1,
+      quote:
+        "Collaborating with Shaurya was an absolute pleasure. His professionalism, promptness, and dedication to delivering exceptional results were evident throughout our project. Shaurya's enthusiasm for every facet of development truly stands out. If you're seeking to elevate your website and elevate your brand, Shaurya is the ideal partner.",
+      name: "Reena Rai",
+      title: "Customer",
+    },
+    {
+        id: 2,
+          quote:
+              "Shaurya has proven to be an exceptional collaborator, consistently demonstrating strong teamwork and communication skills. He approaches each project with a positive attitude and a willingness to listen, making him an invaluable team member. His ability to integrate diverse perspectives and drive collective problem-solving enhances the group's productivity and innovation. Shaurya's reliability and proactive nature ensure that deadlines are met and that the collaborative process is both efficient and enjoyable. Overall, his contributions significantly elevate the quality of the team's output.",
+        name: "Sonal Yadav",
+        title: "Client",
+      },
+  ];
+  
+  export const companies = [
+    {
+      id: 1,
+      name: "UponlyTechnologies",
+      img: "/cloud.svg",
+      nameImg: "/cloudName.svg",
+    },
+  ];
+  
+  export const workExperience = [
+    {
+      id: 1,
+      title: "Frontend Engineer Intern",
+      desc: "Assisted in the development of a web-based platform using React.js, enhancing interactivity.",
+      className: "md:col-span-2",
+      thumbnail: "/exp1.svg",
+    },
+    {
+      id: 2,
+      title: "Copy&Content Writing",
+      desc: "Designed a PDF document for my client as per his instructions regarding stocks.",
+      className: "md:col-span-2", // change to md:col-span-2
+      thumbnail: "/exp2.svg",
+    },
+  ];
+  
+  export const socialMedia = [
+    {
+      id: 1,
+      img: "/git.svg",
+    },
+    {
+      id: 2,
+      img: "/twit.svg",
+    },
+    {
+      id: 3,
+      img: "/link.svg",
+    },
+  ];
